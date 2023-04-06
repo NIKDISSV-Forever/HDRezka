979e33336f46a7ed6c8bf53b18e9e43604bda983
NIKDISSV-Forever <nikdissv.github.forever@gmail.com>
2023-04-06 18:59:12 +0600

    see changelog

---
+++ README.md
@@ -0,0 +1,19 @@
+[HDRezka](https://rezka.ag/) site API.
+
+# Install
+
+> `pip install HDRezka`
+
+# Example
+
+```python
+from hdrezka import *
+
+player = Search('Avatar')[1, 0].player
+print(player.post.info)
+print(player.get_stream().best_url)
+```
+
+# [Documentation](https://nikdissv-forever.github.io/HDRezka/hdrezka)
+
+# [GitHub](https://github.com/NIKDISSV-Forever/HDRezka)
--- setup.py
+++ setup.py
@@ -21,7 +21,10 @@ setuptools.setup(
     long_description_content_type='text/markdown',
 
     url='https://github.com/NIKDISSV-Forever/HDRezka',
-
+    project_urls={
+        'GitHub': 'https://github.com/NIKDISSV-Forever/HDRezka',
+        'Documentation': 'https://nikdissv-forever.github.io/HDRezka/hdrezka'
+    },
     packages=setuptools.find_packages(),
     install_requires=install_requires,
 
