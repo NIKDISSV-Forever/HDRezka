09314ea97e583305a0997c72b370e88d76f1be5c
NIKDISSV-Forever <nikdissv.github.forever@gmail.com>
2023-04-06 18:56:00 +0600

    see changelog

--- hdrezka/__init__.py
+++ hdrezka/__init__.py
@@ -14,6 +14,8 @@ player = Search('Avatar')[1, 0].player
 print(player.post.info)
 print(player.get_stream().best_url)
 ```
+
+# [GitHub](https://github.com/NIKDISSV-Forever/HDRezka)
 """
 from .api import *
 from .post import *
